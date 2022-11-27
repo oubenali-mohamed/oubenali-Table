5cdb50be3fb754ac9e611eabfc8d63c501b1f6ad
oubenali mohamed <oubenali15@gmail.com>
2022-11-27 23:08:12 +0100

    modification du tbody du table

--- src/lib/components/TableEmployee.js
+++ src/lib/components/TableEmployee.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function TableEmployee({ columns, row }) {
+function TableEmployee({ columns, rows }) {
   return (
     <div>
       <table>
@@ -12,17 +12,19 @@ function TableEmployee({ columns, row }) {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{row.firstName}</td>
-            <td>{row.lastName}</td>
-            <td>{row.city}</td>
-            <td>{row.zipCode}</td>
-            <td>{row.street}</td>
-            <td>{row.state}</td>
-            <td>{row.department}</td>
-            <td>{row.dateOfBirth}</td>
-            <td>{row.startDate}</td>
-          </tr>
+          {rows.map((row, index) => (
+            <tr key={index}>
+              <td>{row.firstName}</td>
+              <td>{row.lastName}</td>
+              <td>{row.city}</td>
+              <td>{row.zipCode}</td>
+              <td>{row.street}</td>
+              <td>{row.state}</td>
+              <td>{row.department}</td>
+              <td>{row.dateOfBirth}</td>
+              <td>{row.startDate}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
